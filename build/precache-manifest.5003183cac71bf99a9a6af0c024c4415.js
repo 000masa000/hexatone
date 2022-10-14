38ddfc0246495e6a46794c86bbe6395272c5101a
Marc Sabat <masa@plainsound.org>
2022-10-14 23:30:45 +0300

    replaced micro-tonal with microtonal

---
+++ build/precache-manifest.5003183cac71bf99a9a6af0c024c4415.js
@@ -0,0 +1,409 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61f453155253824109a4bdab116f63d",
+    "url": "1024.png"
+  },
+  {
+    "revision": "1e906c51356fc27c2b05d0e959cfbe14",
+    "url": "192.png"
+  },
+  {
+    "revision": "facbf78c2e821a69df15b1eebf358610",
+    "url": "256.png"
+  },
+  {
+    "revision": "a77cb71b5ef06e1ab10f975859fcdedc",
+    "url": "512.png"
+  },
+  {
+    "url": "a0e1078dddf83bfb119d86249e9f1e5e.png"
+  },
+  {
+    "revision": "0e5fc39649778ae136dc518f73b206c1",
+    "url": "index.html"
+  },
+  {
+    "revision": "ebeae72f3fc44f83ff5a",
+    "url": "main.css"
+  },
+  {
+    "revision": "ebeae72f3fc44f83ff5a",
+    "url": "main.js"
+  },
+  {
+    "revision": "3df54bba2137ec524f3fb39f2c61461a",
+    "url": "main.js.LICENSE"
+  },
+  {
+    "revision": "450996564a494e14047bcbe67c46e9e5",
+    "url": "manifest.webmanifest"
+  },
+  {
+    "revision": "09da7774a2b2d6ace47d0f6c3c7aef87",
+    "url": "sounds/WMRI-in6-har7-110.mp3"
+  },
+  {
+    "revision": "23ab2d5a5edaa00ed4224954445ec2a8",
+    "url": "sounds/WMRI-in6-har7-220.mp3"
+  },
+  {
+    "revision": "21fa518b3bd546602b30665e0b9eae22",
+    "url": "sounds/WMRI-in6-har7-440.mp3"
+  },
+  {
+    "revision": "4816e34b9c2cac52e8876b951a3deb35",
+    "url": "sounds/WMRI-in6-har7-880.mp3"
+  },
+  {
+    "revision": "0c23241504e47657a4ddda6f7ea04470",
+    "url": "sounds/WMRI-in7-har6-110.mp3"
+  },
+  {
+    "revision": "1725c55e035ecb8086f8f656e34cae6e",
+    "url": "sounds/WMRI-in7-har6-220.mp3"
+  },
+  {
+    "revision": "ad315ce570fed463b119fdeb27c8d76f",
+    "url": "sounds/WMRI-in7-har6-440.mp3"
+  },
+  {
+    "revision": "5afe376afa86e90a5e55818e2e6bf545",
+    "url": "sounds/WMRI-in7-har6-880.mp3"
+  },
+  {
+    "revision": "9d4a57f8a4904235a5c8b69d07dc465a",
+    "url": "sounds/WMRI11LST110.mp3"
+  },
+  {
+    "revision": "5d66b8235aa5676e407969f92125f419",
+    "url": "sounds/WMRI11LST220.mp3"
+  },
+  {
+    "revision": "4f462dde5efe342f06ea77716c425354",
+    "url": "sounds/WMRI11LST440.mp3"
+  },
+  {
+    "revision": "e56600a5053d90d80d0bbfdbd9142dc7",
+    "url": "sounds/WMRI11LST880.mp3"
+  },
+  {
+    "revision": "55e97c3fb7f1ee6062a7564e6b0578ef",
+    "url": "sounds/WMRI13LST110.mp3"
+  },
+  {
+    "revision": "5fffbc655ea3faeb6cf4e62eb57f45a7",
+    "url": "sounds/WMRI13LST220.mp3"
+  },
+  {
+    "revision": "55fdf7e308fd4d69b5a87b2c7e6cd1d9",
+    "url": "sounds/WMRI13LST440.mp3"
+  },
+  {
+    "revision": "504e89f436d1dc5c19a6e74ca8f44156",
+    "url": "sounds/WMRI13LST880.mp3"
+  },
+  {
+    "revision": "321cfdb39e0899e9c1b2880e5e8c66e5",
+    "url": "sounds/WMRI3LST110.mp3"
+  },
+  {
+    "revision": "306570dcbd2f42b629194b7f3a311b3c",
+    "url": "sounds/WMRI3LST220.mp3"
+  },
+  {
+    "revision": "9fe01e48cd32b0ddb40f4e0905197456",
+    "url": "sounds/WMRI3LST440.mp3"
+  },
+  {
+    "revision": "fe7f1f83c1bb7de13365ff247ea29d79",
+    "url": "sounds/WMRI3LST880.mp3"
+  },
+  {
+    "revision": "7ad70edfaeae71ebef220a39ca29c377",
+    "url": "sounds/WMRI5LST110.mp3"
+  },
+  {
+    "revision": "9f4819d3c8e0e793335a95cd902db655",
+    "url": "sounds/WMRI5LST220.mp3"
+  },
+  {
+    "revision": "01f2805ee131db6f16df582bc6052b43",
+    "url": "sounds/WMRI5LST440.mp3"
+  },
+  {
+    "revision": "f23a2425ed591ccb0049fa436f057f9c",
+    "url": "sounds/WMRI5LST880.mp3"
+  },
+  {
+    "revision": "1e8f34ff3a8d12d0ba262a9dec0f693f",
+    "url": "sounds/WMRI5Lpike110.mp3"
+  },
+  {
+    "revision": "97cbce5514ab83655d5ab9a681922192",
+    "url": "sounds/WMRI5Lpike220.mp3"
+  },
+  {
+    "revision": "e46b688a32bb25b6cd6083b2dc749010",
+    "url": "sounds/WMRI5Lpike440.mp3"
+  },
+  {
+    "revision": "52102aa7503f9a88375b0a1c18ad0953",
+    "url": "sounds/WMRI5Lpike880.mp3"
+  },
+  {
+    "revision": "c0ceb9b8579946afcd472f12858b0673",
+    "url": "sounds/WMRI7LST110.mp3"
+  },
+  {
+    "revision": "7cd11f5b98490e6326ef6c3eadc44f68",
+    "url": "sounds/WMRI7LST220.mp3"
+  },
+  {
+    "revision": "fc253e9734d54ff8d3b63f47eff2ccee",
+    "url": "sounds/WMRI7LST440.mp3"
+  },
+  {
+    "revision": "46d8c426c7cd81abc6f8b10126a4cb1c",
+    "url": "sounds/WMRI7LST880.mp3"
+  },
+  {
+    "revision": "77a449b3ad50f430fbf00f883cd12453",
+    "url": "sounds/WMRIByzantineST110.mp3"
+  },
+  {
+    "revision": "fd70c6519d481082ae2d41e057be4b7f",
+    "url": "sounds/WMRIByzantineST220.mp3"
+  },
+  {
+    "revision": "cc5ec636cc5f10e746aa6308d150c911",
+    "url": "sounds/WMRIByzantineST440.mp3"
+  },
+  {
+    "revision": "c6dc2178ec4f67ce83c1e872a6561d7d",
+    "url": "sounds/WMRIByzantineST880.mp3"
+  },
+  {
+    "revision": "5b48a243c2febc28aa76980625b9be2d",
+    "url": "sounds/WMRInLST110.mp3"
+  },
+  {
+    "revision": "988381e4984e2016fab0188f57a52fa9",
+    "url": "sounds/WMRInLST220.mp3"
+  },
+  {
+    "revision": "53073a081db3e3f70f5d46c14fb179a0",
+    "url": "sounds/WMRInLST440.mp3"
+  },
+  {
+    "revision": "f03cdac36d751e9d722fff2a4561eed7",
+    "url": "sounds/WMRInLST880.mp3"
+  },
+  {
+    "revision": "a18c3674d8853e1b20f1038a76860793",
+    "url": "sounds/choir110.mp3"
+  },
+  {
+    "revision": "2657daea8e2445b275e06fe0a4bd7588",
+    "url": "sounds/choir220.mp3"
+  },
+  {
+    "revision": "e71118681e7cf95c987e9f22ab921656",
+    "url": "sounds/choir440.mp3"
+  },
+  {
+    "revision": "3686118aee9e5721c99f6c568b61e83d",
+    "url": "sounds/choir880.mp3"
+  },
+  {
+    "revision": "929d1054b41b6736dee65263786d4136",
+    "url": "sounds/gayageum110.mp3"
+  },
+  {
+    "revision": "52c0962d9b3d092b16c0d10bd6d5af98",
+    "url": "sounds/gayageum220.mp3"
+  },
+  {
+    "revision": "fb5d172ff1c7d12016b665d385bfd67e",
+    "url": "sounds/gayageum440.mp3"
+  },
+  {
+    "revision": "40a70479049a30464a61aa4d7a40b7ea",
+    "url": "sounds/gayageum880.mp3"
+  },
+  {
+    "revision": "6187343b6d6108ca8d9c8ea842dcdd82",
+    "url": "sounds/harp110.mp3"
+  },
+  {
+    "revision": "fbc151573fdb45114ad351d907ef4ed0",
+    "url": "sounds/harp220.mp3"
+  },
+  {
+    "revision": "2d80e8bd2db94b33d469bfd56971e06d",
+    "url": "sounds/harp440.mp3"
+  },
+  {
+    "revision": "545624a7e9481ca6a9655c0564c3b2dd",
+    "url": "sounds/harp880.mp3"
+  },
+  {
+    "revision": "fdffb7691c70bbef261778c904407b4e",
+    "url": "sounds/harpsichord110.mp3"
+  },
+  {
+    "revision": "61b72b1b675a3a961d7e018e3f87b1d1",
+    "url": "sounds/harpsichord220.mp3"
+  },
+  {
+    "revision": "b7f65592cdbabde135d3936564dc1f80",
+    "url": "sounds/harpsichord440.mp3"
+  },
+  {
+    "revision": "692873a674e32455bc6377a4f113f9af",
+    "url": "sounds/harpsichord880.mp3"
+  },
+  {
+    "revision": "5115c06e1f35b9228c2a4eb21025e345",
+    "url": "sounds/marimba110.mp3"
+  },
+  {
+    "revision": "49bd77455fe83b84ba6c379226e23b98",
+    "url": "sounds/marimba220.mp3"
+  },
+  {
+    "revision": "d35dd16d896bf7b3d6326986674fdb94",
+    "url": "sounds/marimba440.mp3"
+  },
+  {
+    "revision": "f03f6df56cdb0789673ee7a1b932d6e8",
+    "url": "sounds/marimba880.mp3"
+  },
+  {
+    "revision": "a9a9d9ad3827117cbe6480610aba24d6",
+    "url": "sounds/musicbox110.mp3"
+  },
+  {
+    "revision": "259ca3a119e438ddddc737f56afc19c0",
+    "url": "sounds/musicbox220.mp3"
+  },
+  {
+    "revision": "418fdea45c82c6967c9e17d58b7aa155",
+    "url": "sounds/musicbox440.mp3"
+  },
+  {
+    "revision": "748176533fc1e9920dcd670b8b644552",
+    "url": "sounds/musicbox880.mp3"
+  },
+  {
+    "revision": "47da905ec19d5859561370f34b416c2e",
+    "url": "sounds/organ110.mp3"
+  },
+  {
+    "revision": "3bcb92387ff59eb5d1ec2319e8d73726",
+    "url": "sounds/organ220.mp3"
+  },
+  {
+    "revision": "a1070e5c9fc39aa6a672edda611a46fc",
+    "url": "sounds/organ440.mp3"
+  },
+  {
+    "revision": "1eb263312b661e941b7ff810e44f303b",
+    "url": "sounds/organ880.mp3"
+  },
+  {
+    "revision": "74faf1e6393d86f4363641f86ad5ab91",
+    "url": "sounds/organleslie110.mp3"
+  },
+  {
+    "revision": "1859170a254af357a93cba25d241cc45",
+    "url": "sounds/organleslie220.mp3"
+  },
+  {
+    "revision": "6f8ecf8257a65a36211453e4e408fc9b",
+    "url": "sounds/organleslie440.mp3"
+  },
+  {
+    "revision": "9edb4b3b4f49b5de01bee531ab94407c",
+    "url": "sounds/organleslie880.mp3"
+  },
+  {
+    "revision": "533b4071598c450b5d0112a2cbfbde8e",
+    "url": "sounds/piano110.mp3"
+  },
+  {
+    "revision": "53a7bdec8da7742e06dcd686bcf60250",
+    "url": "sounds/piano220.mp3"
+  },
+  {
+    "revision": "92443f9a04b78b79144297edc5c42ad4",
+    "url": "sounds/piano440.mp3"
+  },
+  {
+    "revision": "81b45c5c1abea5862ee351abd608d3aa",
+    "url": "sounds/piano880.mp3"
+  },
+  {
+    "revision": "d6e6b3f6b3de3bfe2b14c7a44b3e2b53",
+    "url": "sounds/qanun110.mp3"
+  },
+  {
+    "revision": "8be6ca9e6317b98280d98a6ec3971a5f",
+    "url": "sounds/qanun220.mp3"
+  },
+  {
+    "revision": "36dbbc22c6a42d8aca55d72e6301a33c",
+    "url": "sounds/qanun440.mp3"
+  },
+  {
+    "revision": "824d4b9c0c8829947baac065f6f11797",
+    "url": "sounds/qanun880.mp3"
+  },
+  {
+    "revision": "ad1e1aa2155438437b491d5a9d986152",
+    "url": "sounds/rhodes110.mp3"
+  },
+  {
+    "revision": "3343688f6f6f8cc9b24bcc6c685e753f",
+    "url": "sounds/rhodes220.mp3"
+  },
+  {
+    "revision": "caadf02730e0a607ece36873f9cd6d58",
+    "url": "sounds/rhodes440.mp3"
+  },
+  {
+    "revision": "29137ed3ed4fb8c5cc9e3d62c7e33772",
+    "url": "sounds/rhodes880.mp3"
+  },
+  {
+    "revision": "1c358dc1563768702534dd79a07c7f60",
+    "url": "sounds/sawtooth110.mp3"
+  },
+  {
+    "revision": "2bee109dbaa9b448c22d78f3722fe3d3",
+    "url": "sounds/sawtooth220.mp3"
+  },
+  {
+    "revision": "6f3cfea39fae5c51fb538a0f4bd85cda",
+    "url": "sounds/sawtooth440.mp3"
+  },
+  {
+    "revision": "8200b4016dbeb750f8e54c4abaf8ac7a",
+    "url": "sounds/sawtooth880.mp3"
+  },
+  {
+    "revision": "db38edb1d6ba3586250829efb78b9426",
+    "url": "sounds/strings110.mp3"
+  },
+  {
+    "revision": "e5f2137500e41830280945b7a678f7ac",
+    "url": "sounds/strings220.mp3"
+  },
+  {
+    "revision": "e0c94a95f06f1a51b01a11366cd1670b",
+    "url": "sounds/strings440.mp3"
+  },
+  {
+    "revision": "4a3d3ee96402a48524c7f8fdc6d77436",
+    "url": "sounds/strings880.mp3"
+  }
+]);
