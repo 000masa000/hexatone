b583e271c458e5e62beb54cb020a0eb6edd50551
Marc Sabat <masa@plainsound.org>
2022-11-28 11:40:59 +0200

    credit update

--- src/blurb.js
+++ src/blurb.js
@@ -9,7 +9,7 @@ const Blurb = () => (
       WebApp developed by <a href="http://jamesfenn.com/" target="new">James Fenn</a> with additions and modifications by <a href="http://brandlew.com/" target="new">Brandon Lewis</a>, <a href="http://whatmusicreallyis.com/" title="What Music Really İs" target="new">Bo Constantinsen</a>, <a href="https://sites.google.com/site/wangchengu/" target="new">Chengu Wang</a>, <a href="https://ashton.snelgrove.science">Ashton Snelgrove</a>, and <a href="https://www.plainsound.org"  target="new">Marc Sabat</a>.
     </p>
     <p>
-      Credits to Scott Thompson and <a href="http://ozanyarman.com/" target="new">Dr. Ozan Yarman</a> for contributing samples.
+      Credits to Scott Thompson, Tim Kahn, Carlos Vaquero, and <a href="http://ozanyarman.com/" target="new">Dr. Ozan Yarman</a> for contributing samples.
     </p>
     <p>
       Current version 2.1 (Nov. 2022), released as Free/Libre and Open Source Software under <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" target="new">GPL-3.0</a>. Code can be found on <a href="https://github.com/000masa000/terpstrakeyboard" target="new">GitHub</a>.
